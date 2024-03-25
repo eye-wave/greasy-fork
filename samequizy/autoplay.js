589f9ee8175f1a6fd2132cba0fe8b2cfb4105c12
_eyewave <89079979+eye-wave@users.noreply.github.com>
2024-03-25 20:06:13 +0100

    feat: refactor samequizy/autoplay.js

--- samequizy/autoplay.js
+++ samequizy/autoplay.js
@@ -5,27 +5,11 @@
 // @match          https://samequizy.pl/*
 // @icon           https://www.google.com/s2/favicons?sz=64&domain=samequizy.pl
 // @grant          none
-// @version        2.0.2
+// @version        3.0.0
 // @author         eye-wave
 // @license GPL    3.0
 // @description    wyłącza automatyczne załączanie nowego quizu
 // @description:en Disables the automatic loading of new quizzes on samequizy.pl.
 // ==/UserScript==
 
-const clickCancel = () => document.querySelector(".user-action.cancel")?.click()
-
-function handleMutations(mutationsList) {
-  for (const mutation of mutationsList) {
-    if (mutation.type === "childList") {
-      if (document.querySelector(".result-container")?.style?.display !== "none") {
-        clickCancel()
-        break
-      }
-    }
-  }
-}
-
-new MutationObserver(handleMutations).observe(document.body, {
-  childList: true,
-  subtree: true,
-})
+document.querySelector(".next-related")?.remove()
