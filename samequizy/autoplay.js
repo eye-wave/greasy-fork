04a7264011305a94b745e5cb3ff141767baa82e0
_eyewave <89079979+eye-wave@users.noreply.github.com>
2024-03-27 16:46:02 +0100

    feat: autoplay.js add mutation observer

--- samequizy/autoplay.js
+++ samequizy/autoplay.js
@@ -5,11 +5,21 @@
 // @match          https://samequizy.pl/*
 // @icon           https://www.google.com/s2/favicons?sz=64&domain=samequizy.pl
 // @grant          none
-// @version        3.0.0
+// @version        3.0.1
 // @author         eye-wave
 // @license GPL    3.0
 // @description    wyłącza automatyczne załączanie nowego quizu
 // @description:en Disables the automatic loading of new quizzes on samequizy.pl.
 // ==/UserScript==
 
-document.querySelector(".next-related")?.remove()
+const removeNextRelated = () => document.querySelector(".next-related")?.remove()
+removeNextRelated()
+
+new MutationObserver(mutationsList => {
+  for (const mutation of mutationsList) {
+    if (mutation.type === "childList") removeNextRelated()
+  }
+}).observe(document.body, {
+  childList: true,
+  subtree: true,
+})
