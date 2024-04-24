d3d7f98c1e1d30e050357b3255f93a4dc6998718
_eyewave <89079979+eye-wave@users.noreply.github.com>
2024-04-24 09:32:15 +0200

    chore: refactor

--- packages/samequizy-solver/src/index.js
+++ packages/samequizy-solver/src/index.js
@@ -1,3 +1,6 @@
+import quitIcon from "./icons/quit.svg"
+import randomIcon from "./icons/random.svg"
+
 const IMAGE_HEIGHT = 96
 
 function createButton() {
@@ -76,20 +79,10 @@ function createModal(quizdata) {
   })
 
   elements.push(
-    createAnswerElement(
-      `<svg height="${IMAGE_HEIGHT}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><g fill="currentColor" fill-rule="evenodd"><path d="M48 88c0-4 3-10 7-12l66-40c4-2 10-2 14 0l66 40c4 2 7 8 7 12v80c0 4-3 10-7 12l-66 40c-4 2-10 2-14 0l-66-40c-4-2-7-8-7-12zm14-6 66 36 62-36-62-37-66 37zm-4 12-1 71c-1 3 1 5 3 6l61 38 1-78S58 92 58 94zm76 114 61-38c2-2 4-5 4-8V93l-65 38z"/><path d="M78 132c-5 3-11 1-14-4s-2-12 3-15 11 0 14 5 2 12-3 14m32 21c-5 3-11 1-14-4-3-6-2-12 3-15s11-1 14 5c3 5 2 12-3 14m-32 16c-5 3-11 1-14-4-3-6-2-12 3-15s11-1 14 5c3 5 2 12-3 14m32 21c-5 3-11 1-14-4-3-6-2-12 3-15s11-1 14 5c3 5 2 12-3 14m78-78c-5-3-11-1-14 5-3 5-2 12 3 14 5 3 11 1 14-4s2-12-3-15m-16 29c-5-3-11 0-14 5s-2 12 3 14c5 3 11 1 14-4s2-12-3-15m-17 28c-5-3-11 0-14 5s-2 12 3 14c5 3 11 1 14-4s2-12-3-15M129 69c-7 0-12 5-12 11s5 11 12 11 11-5 11-11-5-11-11-11"/></g></svg>`,
-      "ans__random",
-      "Losowa"
-    )
+    createAnswerElement(randomIcon.replace(/height="\d+"/, `height="${IMAGE_HEIGHT}"`), "ans__random", "Losowa")
   )
 
-  elements.push(
-    createAnswerElement(
-      `<svg height="${IMAGE_HEIGHT}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="m12 13.4-2.9 2.9q-.3.3-.7.3t-.7-.3q-.3-.3-.3-.7t.3-.7l2.9-2.9-2.9-2.9q-.3-.2-.3-.7t.3-.7q.3-.2.7-.2t.7.2l2.9 3 2.9-3q.2-.2.7-.2t.7.2q.3.3.3.7t-.3.7l-3 2.9 3 2.9q.2.3.2.7t-.2.7q-.3.3-.7.3t-.7-.3z"/></svg>`,
-      "act__quit",
-      ""
-    )
-  )
+  elements.push(createAnswerElement(quitIcon.replace(/height="\d+"/, `height="${IMAGE_HEIGHT}"`), "act__quit", ""))
 
   elements.forEach(element => {
     dialog.append(element)
